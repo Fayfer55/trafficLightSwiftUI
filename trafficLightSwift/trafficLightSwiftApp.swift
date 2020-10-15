4b8eb5a0af7c33a6c4a653ad28e8633257c689d7
Fayfer Kirill <sayfer555@gmail.com>
2020-10-15 19:05:23 +0600

    Initial Commit

---
+++ trafficLightSwift/trafficLightSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightSwiftApp.swift
+//  trafficLightSwift
+//
+//  Created by Кирилл Файфер on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
